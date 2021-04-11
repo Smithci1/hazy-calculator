f2ce965f07f9a3c7d24f77b33992a5a88d965713
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-11 11:01:10 -0400

    updated function that accepts operators, regular numbers,string numbers, and null as 0

--- index.js
+++ index.js
@@ -7,13 +7,16 @@
 //define the functions of the operands.
 const numOperand = (value) => {
     let operators  = [`+`, `-`, `*`, `/`]
-    let number = Number(value)
+    let number = Number(value) || (null === 0)
     let stringNum = parseInt(value)
 
-     return (operators.includes(value) && number === value 
+     return (operators.includes(value)
+             || number === value
              || stringNum === value)
+             
 
 }
-console.log(numOperand(2, '4', '+'))
+console.log(numOperand(2,'3', '4', '+',null))
+
 // define rules.
 // create main function which will do the arrithmetic calculation.
