7e1e1e1f954cd55733d880be5b3154131114a604
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-11 02:53:34 -0400

    created a function that accepts operands, regular numbers, and stringed numbers

---
+++ index.js
@@ -0,0 +1,19 @@
+//define the characters I dont need .
+// const notAllowed = (value) => {
+
+// }
+
+//create an array of operand's we will use.
+//define the functions of the operands.
+const numOperand = (value) => {
+    let operators  = [`+`, `-`, `*`, `/`]
+    let number = Number(value)
+    let stringNum = parseInt(value)
+
+     return (operators.includes(value) && number === value 
+             || stringNum === value)
+
+}
+console.log(numOperand(2, '4', '+'))
+// define rules.
+// create main function which will do the arrithmetic calculation.
